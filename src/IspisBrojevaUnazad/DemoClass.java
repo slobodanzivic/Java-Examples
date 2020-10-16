dff4eb92373f09018856049eb1821732644e6572
slobodan <slobodan.m.zivic@gmail.com>
2020-10-16 23:47:01 +0200

    Adding package IsipisBrojevaUnazad

---
+++ src/IspisBrojevaUnazad/DemoClass.java
@@ -0,0 +1,13 @@
+package IspisBrojevaUnazad;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// Ispisati prirodne brojeve od 5 do 20 unazad.
+
+		for (int i = 20; i >= 5; i--) {
+			System.out.println(i);
+		}
+	}
+
+}
---
+++ src/IspisBrojevaUnazad/DemoClass1.java
@@ -0,0 +1,14 @@
+package IspisBrojevaUnazad;
+
+public class DemoClass1 {
+
+	public static void main(String[] args) {
+		// Napisati program za ispis kvadrata prirodnih brojeva od 1 do 10.
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("Broj je :" + i);
+			System.out.println("Kvadrat broja " + i + " je: " + i * i);
+		}
+	}
+
+}
