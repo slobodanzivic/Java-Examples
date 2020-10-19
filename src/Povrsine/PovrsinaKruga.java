1cf0881f1d759679ace4ef2ffcee88b28c022cdd
slobodan <slobodan.m.zivic@gmail.com>
2020-10-19 14:08:03 +0200

    Adding package Povrsine

---
+++ src/Povrsine/PovrsinaKruga.java
@@ -0,0 +1,16 @@
+package Povrsine;
+
+public class PovrsinaKruga {
+
+	public static void main(String[] args) {
+		// Naci povrsinu kruga precnika 10 i ispisati vrednost na ekranu
+		// za resenje koristiti samo jednu promenljivu
+		
+		double d = 10;
+		
+		d = d/2 * d/2 * 3.14;
+		System.out.println("Povrsina kruga je: " + d);
+
+	}
+
+}
---
+++ src/Povrsine/PovrsinaPravougaonika.java
@@ -0,0 +1,19 @@
+package Povrsine;
+
+public class PovrsinaPravougaonika {
+
+	public static void main(String[] args) {
+		// Odrediti povrsinu pravougaonika stranice
+		// a = 10 i b = 20
+		// Rezultat sacuvati u novoj promenljivoj p i ispisati rezultat u konzoli.
+
+		int a = 10;
+		int b = 20;
+		int p;
+
+		p = a * b;
+		System.out.println("Povrsina pravougaonika je: " + p);
+
+	}
+
+}
